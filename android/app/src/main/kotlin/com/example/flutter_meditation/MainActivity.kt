eaaca92610d1619117b015079f96160ee110f207
Marius Poppel <56595078+mapidev93@users.noreply.github.com>
2023-12-04 17:27:21 +0100

    huge refactoring

--- android/app/src/main/kotlin/com/example/flutter_meditation/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_meditation/MainActivity.kt
@@ -10,82 +10,90 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-
+import android.util.Log
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "htw.berlin.de/public_health/binaural_beats"
+    private val toneGenerator = ToneGenerator()
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        // create the message channel object to play binaural beatss
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            call, result ->
-
+        // create the message channel object to get the caller data from flutter
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             try {
                 val frequencyLeft = call.argument<Double>("frequencyLeft")
                 val frequencyRight = call.argument<Double>("frequencyRight")
 
-                // here we check which method is called
-                if (call.method == "playBinauralBeat" && frequencyLeft != null && frequencyRight != null) {
-
-                    val toneGenerator = ToneGenerator()
-                    toneGenerator.generateTone(frequencyLeft, frequencyRight, context)
-                    result.success(true)
-
-                } else {
-                    result.notImplemented()
+                // here we check which method is called from flutter
+                when (call.method) {
+                    "playBinauralBeat" -> {
+                        playBinauralBeat(frequencyLeft, frequencyRight, result);
+                    }
+                    "stopBinauralBeat" -> {
+                        stopBinauralBeat(result);
+                    }
+                    else -> {
+                        result.notImplemented()
+                    }
                 }
             } catch (e: Exception) {
-                result.error("UNAVAILABLE", "Error generating sound (AudioTrack).", null)
+                result.error("UNAVAILABLE", "Error generating / stopping sound (AudioTrack).", e.toString())
             }
         }
-
     }
 
-    override fun onResume() {
-        super.onResume()
-        //setVolumeControlStream(AudioManager.STREAM_MUSIC)
+    private fun playBinauralBeat(frequencyLeft: Double?, frequencyRight: Double?, result: MethodChannel.Result){
+        if (frequencyLeft != null && frequencyRight != null) {
+            toneGenerator.generateTone(frequencyLeft, frequencyRight, context)
+            result.success(true)
+        } else {
+            result.error("INVALID_ARGUMENTS", "Invalid arguments for playBinauralBeat", null)
+        }
+    }
 
+    private fun stopBinauralBeat(result: MethodChannel.Result){
+        toneGenerator.stopPlayingTone()
+        result.success(true)
     }
 
 }
-
 
 class ToneGenerator(){
 
     private lateinit var audioTrack: AudioTrack
     private var bufferSize = 0
-    private val sampleRate = 44100 // Beispiel-Abtastfrequenz
-    //private var amplitude = 0.5 // Beispiel-Amplitude (0.0 bis 1.0)
+    private val sampleRate = 44100 // Standard-Abtastfrequenz
     private val duration = 3.0 // Beispiel-Dauer des Tons in Sekunden
-    //private lateinit var audioManager: AudioManager
     private var isPlaying = false
 
+    fun stopPlayingTone(){
+        isPlaying = false
+    }
 
     fun generateTone(frequencyLeft: Double, frequencyRight: Double, context: Context) {
-        // Berechne die Puffergröße
-        bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT)
-
+        bufferSize = (sampleRate * duration).toInt()
         val builder = AudioTrack.Builder().setAudioAttributes(AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build())
-        builder.setAudioFormat(AudioFormat.Builder().setSampleRate(sampleRate).setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setAudioFormat(AudioFormat.Builder().setSampleRate(sampleRate)
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
                 .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO).build())
-        builder.setBufferSizeInBytes(bufferSize)
-
+                .setBufferSizeInBytes(bufferSize)
         audioTrack = builder.build()
-
         audioTrack.play()
 
-        isPlaying = true
         val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        startToneGeneration(frequencyLeft, frequencyRight, audioManager)
+    }
+
+    // AudioManager as a parameter is required to make the user able to control the volume while playing the tone
+    private fun startToneGeneration(frequencyLeft: Double, frequencyRight: Double, audioManager: AudioManager){
+
+        isPlaying = true
         val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
 
-        val bufferSize = (sampleRate * duration).toInt()
-
-        // asychrone Ausführung in CoRoutine
+        // asynchrone Ausführung in CoRoutine
         GlobalScope.launch {
             while (isPlaying) {
                 val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
@@ -93,33 +101,10 @@ class ToneGenerator(){
 
                 val pcmData = generateStereoSinWave(bufferSize, frequencyLeft, frequencyRight, amplitude)
                 audioTrack.write(pcmData, 0, bufferSize)
-
-                //delay(500)
             }
         }
-
-        /*var audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-        val amplitude = currentVolume.toFloat() / maxVolume.toFloat()
-        audioTrack.setVolume(amplitude.toFloat())
-        audioTrack.play()
-        audioTrack.setVolume(amplitude.toFloat())
-
-        // Erzeuge und spiele den Ton ab
-        generateAndPlayTone(frequencyLeft, frequencyRight, context)*/
     }
 
-    private fun generateAndPlayTone(frequencyLeft: Double, frequencyRight: Double, context: Context) {
-        // Berechne die Anzahl der Abtastpunkte für die gegebene Dauer
-        val numSamples = (sampleRate * duration).toInt()
-
-        // Erzeuge PCM-Daten für den Ton
-        //val pcmData = generateStereoSinWave(numSamples, frequencyLeft, frequencyRight, context)
-
-        // Spiele die PCM-Daten ab
-        //audioTrack.write(pcmData, 0, numSamples)
-    }
 
     private fun generateStereoSinWave(numSamples: Int, frequencyLeft: Double, frequencyRight: Double, amplitude: Float): ShortArray {
         val pcmData = ShortArray(numSamples * 2)
