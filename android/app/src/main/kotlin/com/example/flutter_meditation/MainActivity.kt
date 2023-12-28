01934764447ccd53ba51be0315d4edb6488afb0a
Christopher Knapp <chris.knapp@live.de>
2023-12-28 19:05:55 +0100

    fix stuttering sound on android

--- android/app/src/main/kotlin/com/example/flutter_meditation/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_meditation/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.flutter_meditation
 import android.content.Context
 import android.media.AudioAttributes
 import android.media.AudioFormat
-import android.media.AudioManager
 import android.media.AudioTrack
+import android.os.Build
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -12,126 +12,130 @@ import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import android.util.Log
+import androidx.annotation.RequiresApi
+import kotlin.math.sin
+import kotlin.math.PI
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "htw.berlin.de/public_health/binaural_beats"
-    private lateinit var toneGenerator :ToneGenerator;
+    private lateinit var toneGenerator: ToneGenerator
     private val LOG_TAG = "FLUTTER_APP"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-
-        // create the message channel object to get the caller data from flutter
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            Log.d(LOG_TAG, "Method channel call registered")
-
             try {
-                val frequencyLeft = call.argument<Double>("frequencyLeft")
-                val frequencyRight = call.argument<Double>("frequencyRight")
-                val duration = call.argument<Double>("duration")
-                Log.d(LOG_TAG, "frequencyLeft: $frequencyLeft, frequencyRight: $frequencyRight, duration: $duration")
-                // here we check which method is called from flutter
                 when (call.method) {
                     "playBinauralBeat" -> {
-                        Log.d(LOG_TAG, "playBinauralBeat called")
-                        playBinauralBeat(frequencyLeft, frequencyRight, duration, result);
+                        val frequencyLeft = call.argument<Double>("frequencyLeft") ?: 0.0
+                        val frequencyRight = call.argument<Double>("frequencyRight") ?: 0.0
+                        val duration = call.argument<Double>("duration") ?: 0.0
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            playBinauralBeat(frequencyLeft, frequencyRight, duration, result)
+                        }
                     }
-                    "stopBinauralBeat" -> {
-                        Log.d(LOG_TAG, "stopBinauralBeat called")
-                        stopBinauralBeat(result);
-                    }
-                    else -> {
-                        result.notImplemented()
-                    }
+                    "stopBinauralBeat" -> stopBinauralBeat(result)
+                    else -> result.notImplemented()
                 }
             } catch (e: Exception) {
-                result.error("UNAVAILABLE", "Error generating / stopping sound (AudioTrack).", e.toString())
+                result.error("UNAVAILABLE", "Error generating/stopping sound.", null)
             }
         }
     }
 
-    private fun playBinauralBeat(frequencyLeft: Double?, frequencyRight: Double?, duration: Double?, result: MethodChannel.Result){
-        if (frequencyLeft != null && frequencyRight != null && duration != null) {
-            toneGenerator = ToneGenerator()
-            toneGenerator.generateTone(frequencyLeft, frequencyRight, duration, context)
-            result.success(true)
-        } else {
-            result.error("INVALID_ARGUMENTS", "Invalid arguments for playBinauralBeat", null)
-        }
-    }
-
-    private fun stopBinauralBeat(result: MethodChannel.Result){
-        toneGenerator.stopPlayingTone()
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun playBinauralBeat(frequencyLeft: Double, frequencyRight: Double, duration: Double, result: MethodChannel.Result) {
+        toneGenerator = ToneGenerator()
+        toneGenerator.generateTone(frequencyLeft, frequencyRight, duration, applicationContext)
         result.success(true)
     }
 
+    private fun stopBinauralBeat(result: MethodChannel.Result) {
+        toneGenerator.stopPlayingTone()
+        result.success(true)
+    }
 }
 
-class ToneGenerator(){
-
-    private lateinit var audioTrack: AudioTrack
-    private var bufferSize = 0
-    private val sampleRate = 44100 // Standard-Abtastfrequenz
+class ToneGenerator {
+    private val sampleRate = 44100
+    private var audioTrack: AudioTrack? = null
     private var isPlaying = false
+    private lateinit var audioThread: Thread
 
-    fun stopPlayingTone(){
-        Log.d("FLUTTER_APP", "Stop coroutine playing sound")
-        isPlaying = false
-    }
-
+    @RequiresApi(Build.VERSION_CODES.M)
     fun generateTone(frequencyLeft: Double, frequencyRight: Double, duration: Double, context: Context) {
-        bufferSize = (sampleRate * duration).toInt()
-        val builder = AudioTrack.Builder().setAudioAttributes(AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA)
-                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).build())
-                .setAudioFormat(AudioFormat.Builder().setSampleRate(sampleRate)
+        val bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT) * 3
+        audioTrack = AudioTrack.Builder()
+            .setAudioAttributes(AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build())
+            .setAudioFormat(AudioFormat.Builder()
+                .setSampleRate(sampleRate)
                 .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO).build())
-                .setBufferSizeInBytes(bufferSize)
-        audioTrack = builder.build()
-        audioTrack.play()
+                .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                .build())
+            .setBufferSizeInBytes(bufferSize)
+            .build()
 
-        val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        startToneGeneration(frequencyLeft, frequencyRight, audioManager)
+        audioTrack?.play()
+        isPlaying = true
+
+        var phaseLeft = 0.0
+        var phaseRight = 0.0
+        val twoPi = 2.0 * Math.PI
+
+        audioThread = Thread {
+            val endTime = System.currentTimeMillis() + (duration * 1000).toLong()
+            while (isPlaying && System.currentTimeMillis() < endTime) {
+                val chunkSize = bufferSize / 6
+                val pcmData = ShortArray(chunkSize * 2) // Stereo: two channels
+
+                for (i in 0 until chunkSize) {
+                    val sampleLeft = sin(phaseLeft).toFloat()
+                    val sampleRight = sin(phaseRight).toFloat()
+
+                    pcmData[i * 2] = (sampleLeft * Short.MAX_VALUE).toInt().toShort()
+                    pcmData[i * 2 + 1] = (sampleRight * Short.MAX_VALUE).toInt().toShort()
+
+                    phaseLeft += twoPi * frequencyLeft / sampleRate
+                    phaseRight += twoPi * frequencyRight / sampleRate
+
+                    // Keep the phase within the 0 to twoPi range
+                    phaseLeft %= twoPi
+                    phaseRight %= twoPi
+                }
+
+                audioTrack?.write(pcmData, 0, pcmData.size)
+            }
+            stopPlayingTone()
+        }
+        audioThread.start()
     }
 
-    // AudioManager as a parameter is required to make the user able to control the volume while playing the tone
-    private fun startToneGeneration(frequencyLeft: Double, frequencyRight: Double, audioManager: AudioManager){
+    fun stopPlayingTone() {
+        synchronized(this) {
+            if (!isPlaying) return
 
-        isPlaying = true
-        val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+            // Flag to stop the audio thread
+            isPlaying = false
 
-        // asynchrone Ausführung in CoRoutine
-        Log.d("FLUTTER_APP", "Start coroutine playing sound")
-        GlobalScope.launch {
-            while (isPlaying) {
-                val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-                val amplitude = currentVolume.toFloat() / maxVolume.toFloat()
-
-                val pcmData = generateStereoSinWave(bufferSize, frequencyLeft, frequencyRight, amplitude)
-
-                if (isPlaying){
-                    audioTrack.write(pcmData, 0, bufferSize)
+            // Wait for the audio thread to finish its current operation
+            if (::audioThread.isInitialized && audioThread.isAlive) {
+                try {
+                    audioThread.join(1000) // Wait for 1 second for the thread to join
+                } catch (e: InterruptedException) {
+                    Thread.currentThread().interrupt() // Restore interrupt status
                 }
             }
+
+            // Stop and release AudioTrack
+            audioTrack?.apply {
+                stop()
+                release()
+            }
+            audioTrack = null
         }
-    }
-
-
-    private fun generateStereoSinWave(numSamples: Int, frequencyLeft: Double, frequencyRight: Double, amplitude: Float): ShortArray {
-        val pcmData = ShortArray(numSamples * 2)
-
-        for (i in 0 until numSamples * 2 step 2) {
-            // amplitude is the volume
-            val sampleLeft = amplitude * Math.sin(2.0 * Math.PI * frequencyLeft * i / (sampleRate * 2.0))
-            val sampleRight = amplitude * Math.sin(2.0 * Math.PI * frequencyRight * i / (sampleRate * 2.0))
-
-            // In pcmData ist wechselt das Ohr bei jeder Arrayposition,
-            // also Muster: samples[0] linkes Ohr, samples[1] rechtes Ohr, samples[2] linkes Ohr ...
-            pcmData[i] = (sampleLeft * Short.MAX_VALUE).toInt().toShort() // Linkes Ohr
-            pcmData[i + 1] = (sampleRight * Short.MAX_VALUE).toInt().toShort() // Rechtes Ohr
-        }
-
-        return pcmData
     }
 
 }
