58436e04f164a907765cbe44101f53a7d86bbbdc
Christopher Knapp <chris.knapp@live.de>
2023-12-29 16:29:37 +0100

    use ListTile component for simplicity and to make the whole row clickable

--- lib/past_sessions/view/screens/past_sessions_page_view.dart
+++ lib/past_sessions/view/screens/past_sessions_page_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_meditation/home/data/model/meditation_model.dart';
-import 'package:flutter_meditation/past_sessions/view/widgets/past_sessions_list_entry_widget.dart';
 import '../../../base/base_view.dart';
+import '../../../common/helpers.dart';
 import '../../view_model/past_sessions_page_view_model.dart';
 
 class PastSessionsPageView extends BaseView<PastSessionsPageViewModel> {
@@ -14,40 +15,42 @@ class PastSessionsPageView extends BaseView<PastSessionsPageViewModel> {
       appBar: AppBar(
         centerTitle: false,
         titleTextStyle: Theme.of(context).textTheme.headlineLarge,
-        backgroundColor: Colors.black,
+        backgroundColor: Theme.of(context).colorScheme.background,
         title: Text(viewModel.pageTitle),
       ),
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.black),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Details',
-                style: TextStyle(
-                  fontSize: 18.0,
-                ),
-              ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: viewModel.meditations?.length ?? 0,
+              itemBuilder: (context, index) {
+                MeditationModel meditation = viewModel.meditations![index];
+                return ListTile(
+                  title: Text('${secondsToHRF(meditation.duration.toDouble())} min'),
+                  titleTextStyle: TextStyle(
+                    fontSize: 17.0,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                  subtitle: Text(timestampToHRF(meditation.timestamp)),
+                  subtitleTextStyle: TextStyle(
+                    fontSize: 15.0,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  trailing: SizedBox(
+                    width: 100, // Adjust the width as needed
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('${viewModel.getAverageHeartRate(meditation).toInt()} BPM'),
+                          const Icon(Icons.chevron_right)
+                        ]),),
+                  onTap: () => viewModel.navigateToSummary(context, meditation),
+                );
+              },
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: viewModel.meditations?.length ?? 0,
-                itemBuilder: (context, index) {
-                  MeditationModel meditation = viewModel.meditations![index];
-                  return PastSessionsListEntry(
-                    meditation: meditation,
-                    onPlayPressed: () {
-                      viewModel.navigateToSummary(context, meditation); 
-                    },
-                    averageBPM: viewModel.getAverageHeartRate(meditation),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
