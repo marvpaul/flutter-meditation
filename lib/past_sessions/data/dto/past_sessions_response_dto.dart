1ddc36d2a2a895d9688ac8acd5db0e8982643955
Christopher Knapp <chris.knapp@live.de>
2023-12-31 15:07:41 +0100

    add domain and data models for past sessions retrieval from middleware

---
+++ lib/past_sessions/data/dto/past_sessions_response_dto.dart
@@ -0,0 +1,58 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'past_sessions_response_dto.freezed.dart';
+part 'past_sessions_response_dto.g.dart';
+
+@freezed
+class PastSessionsResponseDTO with _$PastSessionsResponseDTO {
+  factory PastSessionsResponseDTO({
+    required List<PastSessionDTO> meditationSessions,
+  }) = _PastSessionsResponseDTO;
+
+  factory PastSessionsResponseDTO.fromJson(Map<String, dynamic> json) =>
+      _$PastSessionsResponseDTOFromJson(json);
+}
+
+@freezed
+class PastSessionDTO with _$PastSessionDTO {
+  factory PastSessionDTO({
+    required String date,
+    required String deviceId,
+    required int duration,
+    required bool isCanceled,
+    required bool isCompleted,
+    required List<SessionPeriodDTO> sessionPeriods,
+  }) = _PastSessionDTO;
+
+  factory PastSessionDTO.fromJson(Map<String, dynamic> json) =>
+      _$PastSessionDTOFromJson(json);
+}
+
+
+@freezed
+class SessionPeriodDTO with _$SessionPeriodDTO {
+  factory SessionPeriodDTO({
+    required double beatFrequency,
+    required List<BreathingPatternDTO> breathingPattern,
+    required double breathingPatternMultiplier,
+    required List<int> heartRateMeasurements,
+    required bool isHapticFeedbackEnabled,
+    required String visualization,
+  }) = _SessionPeriodDTO;
+
+  factory SessionPeriodDTO.fromJson(Map<String, dynamic> json) =>
+      _$SessionPeriodDTOFromJson(json);
+}
+
+
+@freezed
+class BreathingPatternDTO with _$BreathingPatternDTO {
+  factory BreathingPatternDTO({
+    int? inhale,
+    int? hold,
+    int? exhale,
+  }) = _BreathingPatternDTO;
+
+  factory BreathingPatternDTO.fromJson(Map<String, dynamic> json) =>
+      _$BreathingPatternDTOFromJson(json);
+}
