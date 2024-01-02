bad63283fb14dae15a3f3ee6cb940a3cfdd336dd
Christopher Knapp <chris.knapp@live.de>
2024-01-02 20:05:04 +0100

    make return type optional for case when model has not been trained yet which is not necessarily an error

--- lib/session/data/repository/session_parameter_optimization_repository.dart
+++ lib/session/data/repository/session_parameter_optimization_repository.dart
@@ -3,7 +3,7 @@ import 'package:flutter_meditation/home/data/model/meditation_model.dart';
 import '../model/session_parameter_optimization.dart';
 
 abstract class SessionParameterOptimizationRepository {
-  Future<SessionParameterOptimization> getSessionParameterOptimization(
+  Future<SessionParameterOptimization?> getSessionParameterOptimization(
       MeditationModel session
       );
   Future<void> trainSessionParameterOptimization(
